31e15daadf4f25ebe5da40fb90dfe472579e6b31
NelsonWang <wangnelson2@gmail.com>
2021-12-25 18:35:21 -0500

    m=laravue | vue | methods

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let app = new Vue({
     el: '#app',
     data: {
-        name: 'Easy Learning'
+        name: 'Easy Learning',
+        message: 'I am 21 years old'
     },
     methods: {
         MyName(){
             return 'easy learning methods'
+        },
+        MyAge(){
+            return `${this.message}`;
         }
     }
 })
