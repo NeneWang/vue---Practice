5aa99ee6dc1c2bdd4dee59fcc85a6b7238e8fc7e
NelsonWang <wangnelson2@gmail.com>
2021-12-25 18:30:59 -0500

    m=laravue | vue | methods

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ let app = new Vue({
     el: '#app',
     data: {
         name: 'Easy Learning'
+    },
+    methods: {
+        MyName(){
+            return 'easy learning methods'
+        }
     }
 })
