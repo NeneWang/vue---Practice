d5b0a0a90d293d012f6f2fa8c6d9539391a043c1
NelsonWang <wangnelson2@gmail.com>
2021-12-25 19:19:15 -0500

    m=laravue | vue | bidning

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ let app = new Vue({
     el: '#app',
     data: {
         name: 'Easy Learning',
-        message: 'I am '
+        message: 'I am ',
+        website: 'https://evildorm.com/',
+        classess1:["hello1","world2"],
     },
     methods: {
         MyName(){
