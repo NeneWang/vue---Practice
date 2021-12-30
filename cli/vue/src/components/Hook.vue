ba669c479726e1fcb1726dde35fb999fd278f3ea
NelsonWang <wangnelson2@gmail.com>
2021-12-30 15:54:47 -0500

    m=laravue | vue | cli | post body titles

--- cli/vue/src/components/Hook.vue
+++ cli/vue/src/components/Hook.vue
@@ -1,3 +1,15 @@
+<template>
+  <div class="hook">
+    <h1>Posts</h1>
+    <hr />
+    <div v-for="post in posts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p> {{post.body}} </p>
+      <div></div>
+    </div>
+  </div>
+</template>
+
 <script>
 import axios from "axios";
 export default {
