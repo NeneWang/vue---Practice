18d15777fc65862aefb1a6e86c92775f01225744
NelsonWang <wangnelson2@gmail.com>
2021-12-30 19:47:51 -0500

    m=laravue | vue | filter serach

--- cli/vue/src/components/Hook.vue
+++ cli/vue/src/components/Hook.vue
@@ -1,35 +1,49 @@
 <template>
   <div class="hook">
     <h1>Posts</h1>
+
     <hr />
-    <div v-for="post in posts" :key="post.id">
+    <input type="text" v-model="seachTrem" placeholder="Search" >
+    <div v-for="post in filtersearch" :key="post.id">
       <h2>{{ post.title }}</h2>
-      <p> {{post.body | snippet }} </p>
+      <p>{{ post.body | snippet }}</p>
       <div></div>
     </div>
   </div>
 </template>
 
+
 <script>
-import axios from "axios";
+import axios from'axios'
 export default {
-  name: "Hook",
-  data() {
-    return {};
-  },
-  methods: {},
-  created() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-        console.log(response);
-        this.posts = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-};
+	name: 'Hook',
+	data(){
+		return {
+        posts:[],
+        seachTrem: ''
+		}
+    },
+    computed:{
+      filtersearch(){
+          return this.posts.filter(post => {
+              return post.title.match(this.seachTrem)
+          })
+          
+      }
+    },
+    methods:{
+         
+    },
+    created(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response =>{
+        this.posts = response.data
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
+}	 
 </script>
 
 <style scoped></style>
